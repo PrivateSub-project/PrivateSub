c9f4e4bd23aaa8e851771d76d3b769dfc1aa7348
kamyab rouhifar <krouhifar@myseneca.ca>
2022-05-24 14:46:14 -0400

    files structure decoration

---
+++ client/private-sub/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Footer, Header, Main } from '../container';
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
