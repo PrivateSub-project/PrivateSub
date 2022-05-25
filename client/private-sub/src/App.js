6d287d74a730f0029da522b8c80f47021efc5c40
kamyab rouhifar <krouhifar@myseneca.ca>
2022-05-24 21:50:46 -0400

    updated app

--- client/private-sub/src/App.js
+++ client/private-sub/src/App.js
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Home from './screens/Home/Home';
 
-function App() {
+export default function App() {
     return (
         <div className="App">
             <Home />
         </div>
     );
 }
-
-export default App;
-=======
-import React from 'react';
-import './App.css';
-
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello Dawgs</h1>
-        </div>
-    );
-}
-
-export default App;
->>>>>>> 5cfbe0f6facb9cfb364a611875c1d46b7a9fb0bd
--- client/private-sub/src/index.js
+++ client/private-sub/src/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -19,13 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 5cfbe0f6facb9cfb364a611875c1d46b7a9fb0bd
