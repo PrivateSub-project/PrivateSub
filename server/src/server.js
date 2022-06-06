b1307ca7052288fbf0c6885dd02df8570a21c851
James Bilik <jbilik@myseneca.ca>
2022-06-06 13:35:36 -0400

    fixed mongo collection issue

--- server/src/server.js
+++ server/src/server.js
@@ -1,55 +1,67 @@
-const stoppable = require('stoppable');
-const { app } = require('./controller');
-const mongoose = require('mongoose');
+const express = require('express');
+const cors = require("cors");
+//const jwt = require('jsonwebtoken');
+//const passport = require("passport");
+//const passportJWT = require("passport-jwt");
+const dotenv = require("dotenv");
 
-const username = 'admin';
-const password = 'privatesub';
-const cluster = 'Cluster0';
-const dbname = 'test';
+dotenv.config();
 
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    age: {
-        type: Number,
-        default: 0,
-    },
+const userService = require("./user-service.js");
+
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// let ExtractJwt = passportJWT.ExtractJwt;
+// let JwtStrategy = passportJWT.Strategy;
+
+// let jwtOptions = {};
+// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+// jwtOptions.secretOrKey = process.env.JWT_SECRET;
+
+// let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+//     console.log('payload received', jwt_payload);
+
+//     if (jwt_payload) {
+//         next(null, { _id: jwt_payload._id, 
+//             userName: jwt_payload.userName,
+//         }); 
+//     }   
+//     else {
+//         next(null, false);
+//     }
+// });
+
+//passport.use(strategy);
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-const userModel = mongoose.model('User', UserSchema);
-
-const HTTP_PORT = parseInt(process.env.PORT || 8080, 10);
-const server = stoppable(
-    app.listen(HTTP_PORT, () => {
-        console.log('Listen to ' + HTTP_PORT);
+app.post("/register", (req, res) => { 
+    userService.registerUser(req.body)
+    .then((msg) =>{
+        res.json({ message: msg });
     })
-);
-
-mongoose
-    .connect(
-        `mongodb+srv://${username}:${password}@cluster0.odr7ei8.mongodb.net/?retryWrites=true&w=majority`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    )
-    .then(() => console.log('mongodb connected'))
-    .catch((err) => console.log(err));
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function () {
-    console.log('Connected successfully');
+    .catch((msg) => {
+        res.status(422).json({ message: msg });
+    });
 });
 
-userModel.find((err, docs) => {
-    if (!err) {
-        console.log(docs);
-    }
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-module.exports = server;
+//app.post("/login")
 
-// Language: javascript
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
