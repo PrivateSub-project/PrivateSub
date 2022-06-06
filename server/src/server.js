eb9cb9e2dcc4d5601bb353051927584d81d00078
James Bilik <jbilik@myseneca.ca>
2022-06-06 13:55:39 -0400

    added login route with jwt

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require("cors");
-//const jwt = require('jsonwebtoken');
-//const passport = require("passport");
-//const passportJWT = require("passport-jwt");
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -13,27 +13,27 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-// let ExtractJwt = passportJWT.ExtractJwt;
-// let JwtStrategy = passportJWT.Strategy;
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
 
-// let jwtOptions = {};
-// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-// jwtOptions.secretOrKey = process.env.JWT_SECRET;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+jwtOptions.secretOrKey = `W$o47GwxOWSweMc1&Hn*pT$C8HBTmcLs`;
 
-// let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-//     console.log('payload received', jwt_payload);
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
 
-//     if (jwt_payload) {
-//         next(null, { _id: jwt_payload._id, 
-//             userName: jwt_payload.userName,
-//         }); 
-//     }   
-//     else {
-//         next(null, false);
-//     }
-// });
+    if (jwt_payload) {
+        next(null, { _id: jwt_payload._id, 
+            userName: jwt_payload.userName,
+        }); 
+    }   
+    else {
+        next(null, false);
+    }
+});
 
-//passport.use(strategy);
+passport.use(strategy);
 app.use(express.json());
 app.use(cors());
 
@@ -51,11 +51,20 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post("/login" , (req, res) => {
+    userService.checkUser(req.body)
+    .then((user) => {
+        var payload = {
+            _id: user._id, 
+            userName: user.userName
+        };
+        const token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({ message: "Login successful", token: token });
+    })
+    .catch((msg) => {
+        res.status(422).json({ message: msg });
+    })
 });
-
-//app.post("/login")
 
 userService.connect()
 .then(() => {
