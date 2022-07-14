95183d918c77edd65639dea9e43372e36643408d
bjam3s <jbilik@myseneca.ca>
2022-07-13 21:16:22 -0400

    conflict fix

--- server/src/models/subscription-model.js
+++ server/src/models/subscription-model.js
@@ -2,35 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const subscriptionSchema = new Schema({
-<<<<<<< HEAD
-  serviceName: {
-    type: String,
-    required: [true, 'Must provide service name'],
-  },
-  startDate: {
-    type: String,
-    required: [true, 'Must provide start date'],
-  },
-  endDate: {
-    type: String,
-    required: [true, 'Must provide end date'],
-  },
-  creditCardId: {
-    type: Number,
-    unique: true,
-    required: [true, 'Must provide credit card identifier'],
-  },
-  user: {
-    type: Number,
-    unique: true,
-    required: [true, 'Must provide user identification'],
-  }
-});
-
-const subscriptionModel = mongoose.model('Subscription', subscriptionSchema);
-
-module.exports = subscriptionModel;
-=======
     brand: {
         type: String,
         required: [true, 'Must provide subscription name']
@@ -62,4 +33,3 @@ module.exports = subscriptionModel;
 const SubscriptionModel = mongoose.model('Subscription', subscriptionSchema);
 
 module.exports = SubscriptionModel;
->>>>>>> 65f81e56379bcb2664bc16e2a0ab746761112d77
--- server/src/services/subscription-service.js
+++ server/src/services/subscription-service.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const logger = require('../logger');
-const subscriptionService = require('../services/subscription-service');
-const subscriptionModel = require('../models/subscription-model');
-
-exports.getSubscriptions = async (req, res) => {
-  try {
-    const subscriptions = await subscriptionModel.find({ user: req.params.id });
-    res.json({
-      message: `A list of subscriptions for user ${req.params.id}`,
-      data: subscriptions,
-      length: subscriptions.length,
-    });
-    } catch (error) {
-    logger.error(error);
-    res.status(500).json(error);
-    }
-}
-
-exports.deleteSubscription = async (req, res) => {
-  try {
-    const subscription = await subscriptionService.deleteSubscription(req.params.id);
-    res.json({
-      message: `Subscription ${req.params.id} deleted`,
-      data: subscription,
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(500).json(error);
-  }
-}
-=======
 const SubscriptionModel = require('../models/subscription-model');
 const logger = require('../logger');
 
@@ -45,4 +13,3 @@ exports.addSubscription = async (req, res) => {
         res.status(500).json({ message: `Error creating subscription `});
     }
 };
->>>>>>> 65f81e56379bcb2664bc16e2a0ab746761112d77
