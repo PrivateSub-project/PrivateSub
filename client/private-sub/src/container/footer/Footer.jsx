679f74d4c9e57e9c3ec5997454bdad9dd134092d
MajdHomsi <majd.homsi.ca@gmail.com>
2022-08-04 21:08:45 -0400

    Updated the footer URL

--- client/private-sub/src/container/footer/Footer.jsx
+++ client/private-sub/src/container/footer/Footer.jsx
@@ -19,7 +19,7 @@ export default function Footer() {
             <div className="gradient__border md:flex md:justify-between relative">
                 <div className="mb-6 md:mb-0">
                     <a
-                        href="https://flowbite.com"
+                        href="https://privatesub-project.web.app/"
                         className="flex items-center"
                     >
                         <img
@@ -84,7 +84,7 @@ export default function Footer() {
             <div className="sm:flex sm:items-center sm:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                     © 2022{' '}
-                    <a href="https://flowbite.com" className="hover:underline">
+                    <a href="https://privatesub-project.web.app/" className="hover:underline">
                         PrivateSub™
                     </a>
                     . All Rights Reserved.
